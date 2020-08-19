62546bbe16f709e2bc65b8f1ed5a3730422b6a42
Pedro Guilherme <guilhermepedro5821@gmail.com>
2020-08-19 00:04:37 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world meu primmeiro programa Java no Relp-it!");
+    System.out.println("Vou enviar para o GitHub etapa 1");
+  }
+}
